cb62bdf9ab1c2c6e07c13d13a437838021e2ce5f
annebelle.dennis@drake.edu <annebelle.dennis@drake.edu>
2026-04-05 16:54:15 -0500

    testing database connection

--- app.py
+++ app.py
@@ -1,6 +1,46 @@
-#touch README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:alexpchin/<reponame>.git
-git push -u origin master
+#source: https://ttsmp3.com/
+#source: https://render.com/docs/deploy-flask
+#source: https://dashboard.render.com/web/srv-d6uu461aae7s73f7qmog/deploys/dep-d6uu46haae7s73f7qn10
+#deployment link: https://dashboard.render.com/
+
+#pip install flask
+#run using "python app.py"
+# pip install psycopg2
+
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    
+    conn = psycopg2.connect(
+        dbname="loan_webapp_database",
+        user="loan_webapp_database_user",
+        password="Tu64h7EbmvRztWE6LW0NUkppdaoENe14",
+        port="5432"
+    )
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE testTable (
+        brand VARCHAR(255),
+        model VARCHAR(255),
+        year INT
+        );"""
+    )
+    cur.execute("""
+        INSERT INTO testTable (brand, model, year)
+        VALUES
+        ('kia', 'forte', 2022),
+        ('camaro', 'camerillo', 2000)"""
+    )
+    cur.execute('SELECT * from testTable')
+    results = cur.fetchall()
+    print(results)
+    cur.execute('drop table testTable')
+    cur.close()
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
