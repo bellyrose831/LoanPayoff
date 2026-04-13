00b90f5233c2b8cec8dca521e9c85e1d9a94ced4
annebelle.dennis@drake.edu <annebelle.dennis@drake.edu>
2026-04-12 20:30:09 -0500

    Add payment function on main page; started building loan details page

--- app.py
+++ app.py
@@ -6,20 +6,24 @@
 
 #run using "python app.py"
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pyairtable import Api
 import pandas as pd
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
     api = Api(os.environ.get("AIRTABLE_API_KEY"))
     table = api.table('appUKKwJXXkEF1xvh', 'tblctK0jy3jj28XhT')
-    response = table.all() 
-    #this comes through as a list of dicts (each record = a dict), want to change to pandas DF
+    
+    if request.method == 'POST':
+        formAnswers = request.form
+        table.create({'Date': formAnswers['date'], 'Amount': int(formAnswers['amount'])})
 
+
+    response = table.all() 
     data = []
     for record in response:
         data.append(record['fields'])
@@ -28,7 +32,11 @@ def main():
 
     data = pd.DataFrame(data)
     print(data)
-    return render_template('home.html', test=data)
+    return render_template('home.html', column_names=data.columns.values,row_data=list(data.values.tolist()), zip=zip)
+
+@app.route('/LoanDetails')
+def details():
+    return render_template('details.html', remBalance = 5000, estPayoffDate = "June 1, 2028", minPayment = 200, interest = 0, extraPayments = 0, nextPaymentAmt = 200, nextPaymentDate = "May 1, 2026")
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
